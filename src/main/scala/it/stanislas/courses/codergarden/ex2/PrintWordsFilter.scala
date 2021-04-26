9f925d44bce854178623cfddcbd8c413e0a859b3
Stanislas Bonifetto <stanislas.bonifetto@gmail.com>
2021-04-26 23:15:13 +0100

    refactor the print in an external object

--- src/main/scala/it/stanislas/courses/codergarden/ex2/PrintWordsFilter.scala
+++ src/main/scala/it/stanislas/courses/codergarden/ex2/PrintWordsFilter.scala
@@ -3,14 +3,14 @@ package it.stanislas.courses.codergarden.ex2
 object PrintWordsFilter {
 
   def print(wordsFiltered: WordsFiltered): Unit = {
-    val shortestWords = wordsFiltered.shortest.mkString(", ")
-    val longestWords = wordsFiltered.longest.mkString(", ")
+    val shortestWordsByComma = wordsFiltered.shortest.mkString(", ")
+    val longestWordsByComma = wordsFiltered.longest.mkString(", ")
 
     val shortestWordLength = lengthOfHead(wordsFiltered.shortest)
     val longestWordLength = lengthOfHead(wordsFiltered.longest)
 
-    println(s"""shortest words are length $shortestWordLength: ${shortestWords}""")
-    println(s"""longest words are length $longestWordLength: ${longestWords}""")
+    println(s"""shortest words are length $shortestWordLength: ${shortestWordsByComma}""")
+    println(s"""longest words are length $longestWordLength: ${longestWordsByComma}""")
   }
 
   private def lengthOfHead(list: List[String]) = list.headOption.getOrElse("").length
