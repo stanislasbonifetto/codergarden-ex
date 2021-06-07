103cecf4853f4b006a92dd9d46aeef7462001d39
Stanislas Bonifetto <stanislas.bonifetto@gmail.com>
2021-06-07 23:05:47 +0100

    refactoring the ExpirableMap with the evict

--- src/main/scala/it/stanislas/courses/codergarden/ex5/ExpirableMap.scala
+++ src/main/scala/it/stanislas/courses/codergarden/ex5/ExpirableMap.scala
@@ -10,29 +10,50 @@ case class ExpirableMap[K, V](
 ) {
 
   def add(key: K, value: V): ExpirableMap[K, V] = {
-    val newInternalMap = internalMap + (key -> MapValue(
-      value,
-      clock.now().plusMillis(ttl.toMillis)
-    ))
-    new ExpirableMap[K, V](newInternalMap, ttl, clock)
+    val (_, newMap) = addOrUpdate(key, value)
+    newMap
   }
 
   def get(key: K): (Option[V], ExpirableMap[K, V]) = {
+    val (maybeValue, map) = getValue(key)
+    (maybeValue.map(_.value), map)
+  }
+
+  def getValue(key: K): (Option[MapValue[V]], ExpirableMap[K, V]) = {
     val maybeValue = internalMap.get(key)
     maybeValue match {
-      case Some(MapValue(value, _)) => {
-        val newInternalMap = internalMap + (key -> MapValue(
-          value,
-          clock.now().plusMillis(ttl.toMillis)
-        ))
-        val newMap = new ExpirableMap[K, V](newInternalMap, ttl, clock)
-        (Some(value), newMap)
-      }
+      case Some(MapValue(value, expiredTime))
+          if clock.now().isBefore(expiredTime) =>
+        val (newValue, newMap) = addOrUpdate(key, value)
+        (Some(newValue), newMap)
       case _ => (None, this)
     }
   }
 
-  def evictCache(): ExpirableMap[K, V] = ???
+  def evictCache(): ExpirableMap[K, V] = {
+    val newInternalMap =
+      internalMap.filter(r => clock.now().isBefore(r._2.expiredTime))
+    val newMap = newExpirableMap(newInternalMap)
+    newMap
+  }
+
+  private def addOrUpdate(
+      key: K,
+      value: V
+  ): (MapValue[V], ExpirableMap[K, V]) = {
+    val newMapValue = newMapValueWithTtl(value)
+    val newInternalMap = internalMap + (key -> newMapValue)
+    (newMapValue, newExpirableMap(newInternalMap))
+  }
+
+  private def newMapValueWithTtl(value: V): MapValue[V] =
+    MapValue(
+      value,
+      clock.now().plusMillis(ttl.toMillis)
+    )
+
+  private def newExpirableMap(internalMap: Map[K, MapValue[V]]) =
+    new ExpirableMap[K, V](internalMap, ttl, clock)
 }
 
 object ExpirableMap {
@@ -41,6 +62,10 @@ object ExpirableMap {
 }
 
 case class MapValue[V](value: V, expiredTime: Instant)
+    extends Ordered[MapValue[V]] {
+  override def compare(that: MapValue[V]): Int =
+    this.expiredTime.compareTo(that.expiredTime)
+}
 
 trait Clock {
   def now(): Instant
--- src/test/scala/it/stanislas/courses/codergarden/ex5/ExpirableMapTest.scala
+++ src/test/scala/it/stanislas/courses/codergarden/ex5/ExpirableMapTest.scala
@@ -12,7 +12,7 @@ class ExpirableMapTest extends AnyFreeSpec with Matchers {
     val expirableMap = ExpirableMap.empty[String, String](5.seconds)
     "can add and element" in {
       val mapWithElement = expirableMap.add("key", "value")
-      mapWithElement should not be (expirableMap)
+      mapWithElement should not be expirableMap
     }
     "can get an element and a new map" in {
       val key = "key"
@@ -20,16 +20,54 @@ class ExpirableMapTest extends AnyFreeSpec with Matchers {
       val mapWithElement = expirableMap.add(key, value)
       val (maybeElement, newMap) = mapWithElement.get(key)
       maybeElement should be(Some(value))
-      newMap should not be (mapWithElement)
+      newMap should not be mapWithElement
     }
-    "can get an element and the ttl is updated" - {}
-    "can't get an element expired" - {}
-    "can't get an element not added" - {}
-    "can evict element expired" - {}
+    "can't get an element not added" in {
+      val (maybeElement, newMap) = expirableMap.getValue("key")
+      maybeElement should be(None)
+      newMap should be(expirableMap)
+    }
+    "can get an element and the ttl is updated" in {
+      val key = "key"
+      val value = "value"
+      val mapWithElement = expirableMap.add(key, value)
+      val (Some(get1), _) = mapWithElement.getValue(key)
+      val (Some(get2), _) = mapWithElement.getValue(key)
+      get2.value should be(get1.value)
+      get2.expiredTime should not be get1.expiredTime
+    }
+  }
+  "An ExpirableMap with a mockClock" - {
+    val ttl = 1.seconds
+    val now = Instant.now()
+    val expiredTime = now.minusSeconds(ttl.toSeconds)
+    val mockClock = new MockClock(expiredTime, now)
+    val expirableMap = ExpirableMap.empty[String, String](ttl, mockClock)
+    "can't get an element expired" in {
+      val key = "key"
+      val value = "value"
+      val mapWithElement = expirableMap.add(key, value)
+      val (maybeElement, newMap) = mapWithElement.get(key)
+      maybeElement should be(None)
+      newMap should be(mapWithElement)
+    }
+  }
+
+  "An ExpirableMap with a mockClock 1" - {
+    val ttl = 1.seconds
+    val now = Instant.now()
+    val expiredTime = now.minusSeconds(ttl.toSeconds)
+    val mockClock = new MockClock(expiredTime, now)
+    val expirableMap = ExpirableMap.empty[String, String](ttl, mockClock)
+    "can evict element expired" in {
+      val mapWithElement = expirableMap.add("key", "value")
+      val newMap = mapWithElement.evictCache()
+      newMap should not be mapWithElement
+    }
   }
 }
 
-class TestClock(times: Instant*) extends Clock {
+class MockClock(times: Instant*) extends Clock {
   private var currentIndex = 0
   override def now(): Instant = {
     assert(currentIndex < times.size, "to many calls")
